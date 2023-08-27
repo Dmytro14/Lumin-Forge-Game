60cbe9d124aa1adf355548199d260437a5d0d7c4
Dmytro14 <922dmytro922@gmail.com>
2023-08-28 00:32:05 +0300

    1st commint

--- Assets/Scripts/FirstTestScript.cs
+++ Assets/Scripts/FirstTestScript.cs
@@ -7,7 +7,7 @@ public class FirstTestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hi");
     }
 
     // Update is called once per frame
